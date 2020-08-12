ffdf59447a6c22b736c6fc5f5e73a83e7a50ff4a
lauren <ljdutton2@icloud.com>
2020-08-12 11:16:25 -0400

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Feed from './components/Feed'
+import Profile from './components/Profile'
+import ArticleView from './components/ArticleView'
+import Editor from './components/Editor'
+import SignInWith from './components/SignInWith'
+class App extends Component {
+    render() {
+        const pathname = window.location.pathname
+        return ( 
+            <div>
+            { !pathname.includes('editor') ? <Header /> : '' }
+            <SignInWith />
+                <Switch>                
+                    <Route exact path="/" component={Feed} />
+                    <Route path="/profile/:id" component={Profile} />
+                    <Route path="/articleview/:id" component={ArticleView} />
+                    <Route path="/editor" component={Editor} />
+                    <Route path="**" component={Feed} />
+                </Switch>
+            </div>
+        );
+    }
 }
-
 export default App;
